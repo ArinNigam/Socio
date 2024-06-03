65def69ab22fd3fd97d755703764421216a26047
Arin Nigam <arin.nigam@gmail.com>
2024-06-03 12:05:06 +0530

    Updated cors

--- server/index.js
+++ server/index.js
@@ -33,9 +33,15 @@ app.use(express.json());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
-// app.use(bodyParser.json({ limit: "30mb", extended: true }));
-// app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors(
+  {
+    origin: "*",
+    methods: ["GET", "POST", "PATCH", "DELETE"],
+    credentials: true,
+  }
+));
 
 app.post("/auth/register", register);
 app.post("/auth/login", login);
